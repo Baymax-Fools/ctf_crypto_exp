4ebb9314f09b78cc8255956562bfe446c2dea719
Baymax-Fools <1254684665@qq.com>
2026-06-09 20:02:13 +0800

    260609

---
+++ ctf/2023/熵密杯/初始谜题.py
@@ -0,0 +1,19 @@
+'''
+请输入谜题服务器IP地址(Please input Puzzle Server IP Address)
+172.10.42.212
+请输入谜题服务器端口号(Please input Puzzle Server Port Number)
+8070
+-----------------------------------------------------
+MSG1:
+e55e3e24a3ae7797808fdca05a16ac15eb5fa2e6185c23a814a35ba32b4637c2
+MAC1:
+0712c867aa6ec7c1bb2b66312367b2c8
+-----------------------------------------------------
+MSG2:
+d8d94f33797e1f41cab9217793b2d0f02b93d46c2ead104dce4bfec453767719
+MAC2:
+43669127ae268092c056fd8d03b38b5b
+-----------------------------------------------------
+请输入您的MSG3(64字节，128个Hex，不要添加空格！)(Please input your 64bytes MSG3(64 bytes,128 hexs,don't using space)):
+'''                                                               
+
